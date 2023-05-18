14446ea4584a035db56a29c8f7b6b1803325905b
dingmh <dingmh@idss-cn.com>
2023-05-18 20:18:20 +0800

    release:a new project for vue3 and ts

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>我是APP</template>
+
+<script setup lang="ts"></script>
+<style scoped lang="less"></style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// const a = 100
+
+// const fu = () => {
+//   console.log(123, '123')
+// }
