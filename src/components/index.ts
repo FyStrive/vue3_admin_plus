3e30faebd27515d915aeea5f094a4cd28f32a706
dingmh <dingmh@idss-cn.com>
2023-05-24 22:57:46 +0800

    feat product: 完成了商品管理模块

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,13 @@
 import { App } from 'vue'
 //引入项目中全部全局组件
 import SvgIcon from './SvgIcon.vue'
+import Pagination from './Pagination/index.vue'
+import Category from './Category/index.vue'
 // 引入element中所有icon图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // console.log(typeof SvgIcon)
 //全局对象
-const allGloablComponent = { SvgIcon }
+const allGloablComponent = { SvgIcon,Pagination, Category }
 
 // 对外暴露插件对象 ，插件对象身上需有install方法
 export default {
---
+++ src/directive/has.ts
@@ -0,0 +1,15 @@
+import pinia from '@/store'
+import useUserStore from '@/store/module/user'
+let userStore = useUserStore(pinia)
+export const isHasButton = (app:any) => {
+    // 获取对应用户仓库
+    // 获取全局自定义指令：实现按钮权限
+    app.directive('has',{
+        // 代表使用这个全局自定义指令的Dom组件挂载完毕会执行一次
+        mounted(el:any,options:any) {
+            if(!userStore.buttons.includes(options.value)){
+                el.parentNode.removeChild(el)
+            }
+        },
+    })
+}
